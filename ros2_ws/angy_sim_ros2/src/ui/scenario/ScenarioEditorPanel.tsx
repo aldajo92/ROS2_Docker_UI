4a7d2aba41d8bb9c4834f712855fe058ed8a7cef
Alejandro Gomez <alejodiscovery20@gmail.com>
2026-05-10 08:39:55 -0500

    fix: style for edit button at scenario editor

--- ros2_ws/angy_sim_ros2/src/ui/scenario/ScenarioEditorPanel.tsx
+++ ros2_ws/angy_sim_ros2/src/ui/scenario/ScenarioEditorPanel.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef, type ChangeEvent, type KeyboardEvent, type
 import { normalizeScenarioFileName } from './ScenarioJsonUtils'
 
 const ICON_PENCIL: ReactNode = (
-  <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
     <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
   </svg>
